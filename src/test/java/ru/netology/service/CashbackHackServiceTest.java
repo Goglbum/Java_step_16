b99b714bc8ddcbfa429e6fddf0169e4d0a97e565
Goglbum <goglbum2115@gmail.com>
2021-01-27 13:07:22 +0300

    failed_test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -25,13 +25,13 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-//    @Test
-//    public void cashbackHackServiceOneThousand() {
-//        CashbackHackService cashbackHackService = new CashbackHackService();
-//        int amount = 1000;
-//        int actual = cashbackHackService.remain(amount);
-//        int expected = 0;
-//        assertEquals(expected, actual);
-//    }
+    @Test
+    public void cashbackHackServiceOneThousand() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 
 }
